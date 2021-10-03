9a977621e72cc290c86749b05d985ba704f2b489
FatmaOzkan <ozkanfatma11@gmail.com>
2021-10-03 16:32:31 -0400

    added wooden spoon test2

---
+++ src/tests2/WoodenSpoonTest2.java
@@ -0,0 +1,11 @@
+package tests2;
+
+public class WoodenSpoonTest2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <  5; i++) {
+            System.out.println("wooden spoon");
+        }
+    }
+}
